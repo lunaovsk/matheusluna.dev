7fb4c22d2bba2ef1d844a9c1cc5c90ce1b06165c
Matheus Luna <matheusluna151634@gmail.com>
2025-12-28 01:52:50 -0300

    chore: configure react-snap for pre-rendering

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+
+if (container.hasChildNodes()) {
+  hydrateRoot(container, (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ));
+} else {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
